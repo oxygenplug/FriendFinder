9fcf59d1fb5b07b62e72683f25455163369c776d
oxygenplug <dan.garces2@gmail.com>
2018-04-26 18:14:11 -0400

    finalized application storage method

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,14 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var JSONStorage = require("node-localstorage").JSONStorage;
-var storage = new JSONStorage("../data");
 
-storage.setItem("users", []);
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var usersList = [];
 // Sets up the Express app to handle data parsing
